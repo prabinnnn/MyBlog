217e7f3d2ed3d88f8350e94a3ab120f4d2f6735b
prabinnnn <prabinbhandari120@gmail.com>
2024-05-26 18:35:32 +0545

    add index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <>
-      <div></div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,5 @@
+const register = () => {
+  return <div>registrater</div>;
+};
+
+export default register;
