46c82c0395c34cb3efbfaedbc4533ff10b7baf7c
prabinnnn <prabinbhandari120@gmail.com>
2024-06-02 01:03:40 +0545

    add index

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,20 @@
-const login = () => {
-  return <div>login</div>;
-};
+function login() {
+  return (
+    <>
+      <div className="col-md-4">
+        <div className="card-shadow">
+          <div className="card-body">
+            <div className="row d-flex justify-content-center align-items-center">
+              <img
+                src="https://previews.123rf.com/images/mrgao/mrgao1502/mrgao150200011/36963437-red-blog-app-icon-illustration-on-white-background.jpg"
+                style={{ width: "75px" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default login;
