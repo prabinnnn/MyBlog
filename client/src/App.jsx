e62fb62f6d822001972ef88936923647cf50817f
prabinnnn <prabinbhandari120@gmail.com>
2024-06-14 22:08:52 +0545

    page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Homepage from "../src/pages/Homepage"
 import ForgetPassword from "./pages/ForgetPassword";
 import UserLayouts from "./layouts/UserLayouts";
 import AdminLayouts from "./layouts/AdminLayouts"
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 function App() {
   return (
     <>
@@ -16,6 +18,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<UserLayouts/>}>
         <Route index element={<Homepage/>} />
+        <Route path="contact" element={<Contact/>} />
+        <Route path="about" element={<About/>} />
         </Route>
         <Route path="/admin" element={<AdminLayouts/>}>
         <Route path="blogs" element={<Homepage/>} />
