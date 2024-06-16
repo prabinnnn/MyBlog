df6a1ce59fe32353e8a8295b29616b641d640a9f
prabinnnn <prabinbhandari120@gmail.com>
2024-06-17 03:42:43 +0545

    layouts

--- client/src/layouts/AdminLayouts.jsx
+++ client/src/layouts/AdminLayouts.jsx
@@ -1,7 +1,20 @@
-
+import { Outlet, useNavigate } from "react-router-dom"
+import AdminNavbar from "./AdminNavbar"
+import { getToken } from "../utils/token"
+import { useEffect } from "react"
 const AdminLayouts = () => {
+  const navigate=useNavigate();
+  useEffect(()=>{
+  const token =getToken();
+  if(!token) navigate('/login')
+  },[navigate])
   return (
-    <div>AdminLayouts</div>
+    <div>
+      <main className="d-flex vh-100">
+      <AdminNavbar/>
+      <Outlet/>
+      </main>
+    </div>
   )
 }
 
